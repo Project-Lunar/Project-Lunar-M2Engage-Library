cf638e7b11558d10416e353c32b8afd8f763bc1d
Swingflip <ryan@ryanhamlin.co.uk>
2020-01-20 21:23:09 +0000

    Initial 1.0.5 commit last manual commit.

---
+++ M2engage/SystemData.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GMWare.M2.Psb;
+
+namespace ProjectLunarUI.M2engage
+{
+    public class SramEntry
+    {
+        public string tag = new string('\0', 8); //byte[8]
+        public int offset = 0;
+        public int size_enc = 0;
+        public int size_dec = 0;
+        public byte[] boundary = new byte[44];
+
+        public static SramEntry FromByteArray(byte[] data)
+        {
+            SramEntry instance = new SramEntry();
+            using (BinaryReader dataReader = new BinaryReader(new MemoryStream(data)))
+            {
+                instance.tag = Encoding.Default.GetString(dataReader.ReadBytes(8));
+                instance.offset = dataReader.ReadInt32();
+                instance.size_enc = dataReader.ReadInt32();
+                instance.size_dec = dataReader.ReadInt32();
+                instance.boundary = dataReader.ReadBytes(44);
+            }
+            return instance;
+        }
+
+        public byte[] ToByteArray()
+        {
+            byte[] data = new byte[64];
+
+            Array.Copy(Encoding.Default.GetBytes(this.tag), 0, data, 0, 8);
+            Array.Copy(BitConverter.GetBytes(this.offset), 0, data, 8, 4);
+            Array.Copy(BitConverter.GetBytes(this.size_enc), 0, data, 12, 4);
+            Array.Copy(BitConverter.GetBytes(this.size_dec), 0, data, 16, 4);
+            Array.Copy(this.boundary, 0, data, 20, 44);
+
+            return data;
+        }
+    }
+
+    public class SramData
+    {
+        public SramEntry[] Sram_Entry = new SramEntry[4];
+        public byte[] Sram_Image = new byte[8192];
+
+        public SramData()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                this.Sram_Entry[i] = new SramEntry();
+            }
+        }
+
+        public static SramData FromByteArray(byte[] data)
+        {
+            SramData instance = new SramData();
+            using (BinaryReader dataReader = new BinaryReader(new MemoryStream(data)))
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    instance.Sram_Entry[i] = SramEntry.FromByteArray(dataReader.ReadBytes(64));
+                }
+                instance.Sram_Image = dataReader.ReadBytes(8192);
+            }
+            return instance;
+        }
+
+        public byte[] ToByteArray()
+        {
+            byte[] data = new byte[8448];
+            for (int i = 0; i < 4; i++)
+            {
+                Array.Copy(Sram_Entry[i].ToByteArray(), 0, data, i * 64, 64);
+            }
+            Array.Copy(Sram_Image, 0, data, 256, 8192);
+
+            return data;
+        }
+    }
+
+    public class SettingGame
+    {
+        public byte[] data = new byte[128];
+    }
+
+    public class SystemData
+    {
+        public int SramCount
+        {
+            get; internal set;
+        }
+
+        public byte[] Base_Settings = new byte[0xEAC];
+        public SettingGame[] Setting_Games;
+        public int[] Work_Trial =  new int[256];
+        public SramData[] Sram_Data;// = new SramData[Alldata.SRAM_Save_Count];
+
+        public SystemData(int sramCount)
+        {
+            SramCount = sramCount;
+            Setting_Games = new SettingGame[sramCount];
+            Sram_Data = new SramData[SramCount];
+
+            for (int i = 0; i < this.SramCount; i++)
+            {
+                this.Setting_Games[i] = new SettingGame();
+                this.Sram_Data[i] = new SramData();
+            }
+        }
+
+        public static SystemData FromByteArray(byte[] data, int sramCount)
+        {
+            SystemData instance = new SystemData(sramCount);
+            using (BinaryReader dataReader = new BinaryReader(new MemoryStream(data)))
+            {
+                instance.Base_Settings = dataReader.ReadBytes(3756);
+                for (int i = 0; i < instance.SramCount; i++)
+                {
+                    instance.Setting_Games[i] = new SettingGame() { data = dataReader.ReadBytes(128) };
+                }
+                for (int i = 0; i < 256; i++)
+                {
+                    instance.Work_Trial[i] = dataReader.ReadInt32();
+                }
+                for (int i = 0; i < instance.SramCount; i++)
+                {
+                    instance.Sram_Data[i] = SramData.FromByteArray(dataReader.ReadBytes(8448));
+                }
+            }
+            return instance;
+        }
+
+        public byte[] ToByteArray()
+        {
+            int offset = 0;
+            byte[] data = new byte[3756 + (SramCount * 128) + 1024 + (SramCount * 8448)];
+
+            Array.Copy(Base_Settings, 0, data, offset, 3756);
+            offset += 3756;
+
+            for (int i = 0; i < SramCount; i++)
+            {
+                Array.Copy(Setting_Games[i].data, 0, data, offset, 128);
+                offset += 128;
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                Array.Copy(BitConverter.GetBytes(Work_Trial[i]), 0, data, offset, 4);
+                offset += 4;
+            }
+            for (int i = 0; i < SramCount; i++)
+            {
+                Array.Copy(Sram_Data[i].ToByteArray(), 0, data, offset, 8448);
+                offset += 8448;
+            }
+
+            return data;
+        }
+    }
+}
---
+++ M2engage/m2engage.cs
@@ -0,0 +1,103 @@
+﻿using GMWare.M2.Psb;
+using Newtonsoft.Json.Linq;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLunarUI.M2engage
+{
+    public static class m2engage
+    {
+        public static string FindPsbKey(string filePath)
+        {
+            string key = string.Empty;
+            BinaryReader reader = new BinaryReader(File.OpenRead(filePath));
+
+            for (int i = 0; i < reader.BaseStream.Length; i += 1024)
+            {
+                byte[] dataBlock = new byte[1024];
+                reader.Read(dataBlock, 0, 1024);
+
+                string blockText = Encoding.Default.GetString(dataBlock);
+                if (blockText.Contains("getExistFileDirInMountArchive"))
+                {
+                    key = blockText.Substring(blockText.IndexOf("getExistFileDirInMountArchive") + 32, 13);
+                    break;
+                }
+            }
+            return key;
+        }
+
+        public static void MigrateSaves(int sourceSize, int destinationSize, string lunarPath = null)
+        {
+            using (ScpClient scp = new ScpClient("169.254.215.100", "root", "5A7213"))
+            {
+                scp.Connect();
+
+                MemoryStream settingsDataStream = new MemoryStream();
+                scp.Download("/usr/game/save/data_008_0000.bin", settingsDataStream);
+
+                MemoryStream settingsMetaStream = new MemoryStream();
+                scp.Download("/usr/game/save/meta_008_0000.bin", settingsMetaStream);
+
+                byte[] settingsData = new byte[settingsDataStream.Length];
+                settingsDataStream.Position = 0;
+                settingsDataStream.Read(settingsData, 0, settingsData.Length);
+                SystemData originalSave = SystemData.FromByteArray(settingsData, sourceSize);
+                SystemData migratedSave = new SystemData(destinationSize);
+
+                migratedSave.Base_Settings = originalSave.Base_Settings;
+                migratedSave.Work_Trial = originalSave.Work_Trial;
+                int savesToCopy = Math.Min(sourceSize, destinationSize);
+                for (int i = 0; i < savesToCopy; i++)
+                {
+                    migratedSave.Setting_Games[i] = originalSave.Setting_Games[i];
+                    migratedSave.Sram_Data[i] = originalSave.Sram_Data[i];
+                }
+
+                byte[] migratedData = migratedSave.ToByteArray();
+
+                using (MemoryStream saveFile = new MemoryStream(migratedData))
+                {
+                    scp.Upload(saveFile, "/usr/game/save/data_008_0000.bin");
+                    if (!string.IsNullOrEmpty(lunarPath))
+                    {
+                        File.WriteAllBytes($@"{lunarPath}\data_008_0000.bin", migratedData);
+                    }
+                }
+
+                settingsMetaStream.Position = 0;
+                using (PsbReader psbReader = new PsbReader(settingsMetaStream))
+                {
+                    JToken meta = psbReader.Root;
+                    meta["FileSize"] = migratedData.Length;
+                    meta["OriginalSize"] = migratedData.Length;
+                    (meta["Digest"] as JStream).BinaryData = MD5.Create().ComputeHash(migratedData);
+
+                    PsbWriter psbWriter = new PsbWriter(meta, null);
+                    psbWriter.Version = psbReader.Version;
+
+                    using (MemoryStream metaFile = new MemoryStream())
+                    {
+                        psbWriter.Write(metaFile, null);
+                        metaFile.Position = 0;
+                        scp.Upload(metaFile, "/usr/game/save/meta_008_0000.bin");
+
+                        if (!string.IsNullOrEmpty(lunarPath))
+                        {
+                            byte[] metaData = new byte[metaFile.Length];
+                            metaFile.Position = 0;
+                            metaFile.Read(metaData, 0, metaData.Length);
+                            File.WriteAllBytes($@"{lunarPath}\meta_008_0000.bin", metaData);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
